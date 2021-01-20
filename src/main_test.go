877ae3e6abe43e090bf87d9f7c4de95cf8524805
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-01-20 14:48:32 +0530

    Update Product unit test

--- src/main_test.go
+++ src/main_test.go
@@ -33,7 +33,8 @@ func Connect() (*sql.DB, error){
 
 	db, err := sql.Open("mysql", "root:Qwerty@2412@tcp(127.0.0.1:3306)/applicationproject")
 
-	return db, err}
+	return db, err
+}
 
 func CreateNewProduct(product Product2) (Product2, error){
 	vid := product.VendorId
@@ -111,6 +112,37 @@ func CreateNewAccount(user User) (User, error){
 
 	return i, err1
 }
+func UpdateProductt(product Product2)(Product2, error){
+	productId := product.ProductId
+	vendorId := product.VendorId
+	quantity := product.ProductQuantity
+	price := product.ProductPrice
+
+	var p Product2
+
+	db1,err:= Connect()
+	if err!= nil{
+		panic(err.Error)
+	}
+
+	res, err := db1.Query("UPDATE product SET product_quantity = ?, product_price = ? WHERE product_id = ? AND vendor_id = ?",quantity,price, productId,vendorId )
+		if err!= nil{
+			panic(err.Error())
+		}
+
+	res.Close()
+
+	row, err := db1.Query("SELECT product_quantity, product_price FROM product WHERE product_id = ? ", productId)
+	if err!= nil{
+		panic(err.Error())
+	}
+	row.Next()
+
+	err1:= row.Scan(&p.ProductQuantity, &p.ProductPrice)
+
+	return p, err1
+
+}
 
 func TestCreateNewAccount(t *testing.T){
 	user := User{Name : "Arjun",Password : "12"}
@@ -147,3 +179,67 @@ func TestCreateNewProduct(t *testing.T){
 	}
 
 }
+
+func TestUpdateProduct(t *testing.T){
+	product := Product2{VendorId : 1, ProductId : 28, ProductName : "Test Product", ProductQuantity : 1, ProductPrice: 10}
+	updatedProduct, err:= UpdateProductt(product)
+	if err!= nil{
+		t.Fatalf(" error: %s", err)
+	} else{
+		fmt.Println("No error")
+	}
+
+	if product.ProductQuantity!= updatedProduct.ProductQuantity || product.ProductPrice != updatedProduct.ProductPrice{
+		t.Fatalf("Did not update th  product")
+	} else{
+		fmt.Println("Product updated successfully")
+	}
+}
+
+/*func TestDeleteProduct(t *testing.T){
+	
+	name := "arjun"
+	password := "1234"
+	Id := 1
+	pid := 2
+	var productName string
+
+
+	db1,err:= Connect()
+
+	if err!= nil{
+		panic(err.Error)
+	}
+
+	ven_ID, err := db1.Query("SELECT id FROM vendor WHERE name = ? AND password = ?", name, password)
+	if err != nil{
+		panic(err.Error)
+	}
+	var id int
+	ven_ID.Next()
+	err = ven_ID.Scan(&id)
+	if err!= nil{
+		panic(err.Error)
+	}
+	if id == Id{
+		res, err := db1.Query("DELETE FROM product WHERE product_id = ?", pid)
+		if err!= nil{
+			panic(err.Error())
+		}
+		res.Close()
+	}
+
+	row, _:= db1.Query("Select product_name FROM product WHERE product_id = ?",id)
+	row.Next()
+	err1:= row.Scan(productName)
+
+	if err1 !=nil{
+		fmt.Println("Row deleted succesfully")
+	}else{
+		t.Fatalf("Did not delete the  product")
+	}
+
+	
+
+	
+}*/
